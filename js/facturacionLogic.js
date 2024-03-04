e939e25d54993159e0aeb514ad4f661fbb535c17
luis.vasquez <luis.vasquez@ingeneo.com.co>
2024-03-03 22:11:07 -0600

    Modificando pantalla de facturacion

--- js/facturacionLogic.js
+++ js/facturacionLogic.js
@@ -10,32 +10,28 @@ function cargarResumeFactura() {
     var subtotalLabel = document.getElementById('subtotal');
     var totalLabel = document.getElementById('total');
 
-    let subTotal = listaCarrito.reduce((sum, itemCarrito) => {
-        return sum + itemCarrito.precioUnitario;
-    }, 0);
-
-    let sumatotal = 5 + subTotal;
-    sumatotal = sumatotal.toFixed(2);
-
-    
-    subtotalLabel.innerHTML = `<span>Sub-total </span> <span class="text-success">$${subTotal}</span>`;
-    totalLabel.innerHTML = `<span>Total </span> <span class="text-success">$${sumatotal}</span>`;
-
-    var divTotal = document.createElement('div');
+    let subTotal = 0;
 
     for(let item of listaCarrito) {
+        let itemValue = item.cantidad * item.precioUnitario;
+        subTotal = subTotal + itemValue;
+        
         var itemDiv = document.createElement('div');
         itemDiv.innerHTML  = `
             <h5 class="mb-3">${item.nombreFunko}</h5>
                 <div>
                   <div class="d-flex justify-content-between">
                     <div class="d-flex flex-row mt-1">
-                      <h6>Precio</h6>
+                      <h6>Precio unitario</h6>
                       <h6 class="fw-bold text-success ms-1">$ ${item.precioUnitario}</h6>
                     </div>
                     <div class="d-flex flex-row mt-1">
                       <h6>Cantidad</h6>
                       <h6 class="fw-bold text-success ms-1"> ${item.cantidad}</h6>
+                    </div>
+                    <div class="d-flex flex-row mt-1">
+                      <h6>Total del producto</h6>
+                      <h6 class="fw-bold text-success ms-1"> ${itemValue}</h6>
                     </div>
                   </div>
                   <div class="p-2 d-flex justify-content-between align-items-center" style="background-color: #eee;">
@@ -47,4 +43,11 @@ function cargarResumeFactura() {
          dataList.before(itemDiv);
     }
 
+    let sumatotal = subTotal + 5;
+
+    subTotal = subTotal.toFixed(2);
+    sumatotal = sumatotal.toFixed(2);
+
+    subtotalLabel.innerHTML = `<span>Sub-total </span> <span class="text-success">$${subTotal}</span>`;
+    totalLabel.innerHTML = `<span>Total </span> <span class="text-success">$${sumatotal}</span>`;
 }
